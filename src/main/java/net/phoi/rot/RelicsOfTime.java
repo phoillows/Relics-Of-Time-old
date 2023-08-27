e0478e78f49d2db4a4e269cae4d2c2aca26853f3
aggustinn <aalnahas.lego@gmail.com>
2023-08-27 11:48:26 +0300

    Basic mod workspace setup

---
+++ src/main/java/net/phoi/rot/RelicsOfTime.java
@@ -0,0 +1,45 @@
+package net.phoi.rot;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RelicsOfTime.MODID)
+public class RelicsOfTime {
+    public static final String MODID = "rot";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public RelicsOfTime() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);  // Register the commonSetup method for modloading
+
+        MinecraftForge.EVENT_BUS.register(this);  // Register ourselves for server and other game events we are interested in
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
